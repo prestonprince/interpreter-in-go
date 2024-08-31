dcbe7d19abd927dffb4957907bea53b6b63f5125
Preston Prince <prestonprince18@gmail.com>
2024-08-30 23:43:50 -0400

    define lexer struct New func

---
+++ src/lexer/lexer.go
@@ -0,0 +1,14 @@
+package lexer
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	return &Lexer{
+		input: input,
+	}
+}
